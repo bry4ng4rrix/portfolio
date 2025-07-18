65d4a45de74483c5fb0713bea8046a835bc13c0c
Bry4ng4rrix <bryanmfb4@gmail.com>
2025-07-18 12:16:29 +0300

    .

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -102,7 +102,7 @@ const portfolio = () => {
       {/* Navigation */}
       <nav className="fixed top-0 w-full bg-background/50 backdrop-blur-md justify-center items-center z-50">
         <div className="max-w-7xl mx-auto ">
-          <div className="flex justify-between items-center py-2">
+          <div className="flex px-3 justify-between items-center py-2">
             <div className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
              BGarrix
             </div>
