aabd2c7fd0c8139b98762332b88e0baa87b2dbb5
Bry4ng4rrix <bryanmfb4@gmail.com>
2025-07-18 10:21:34 +0300

    a

--- .next/server/chunks/ssr/[root-of-the-server]__03c50571._.js
+++ .next/server/chunks/ssr/[root-of-the-server]__03c50571._.js
@@ -810,7 +810,7 @@ const portfolio = ()=>{
                                             lineNumber: 107,
                                             columnNumber: 13
                                         }, this),
-                                        "  BG Dev"
+                                        "  BGarrix"
                                     ]
                                 }, void 0, true, {
                                     fileName: "[project]/src/app/page.jsx",
@@ -818,7 +818,7 @@ const portfolio = ()=>{
                                     columnNumber: 13
                                 }, this),
                                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-                                    className: "hidden md:flex space-x-2 text-foreground  ",
+                                    className: "hidden md:flex  text-foreground  ",
                                     children: [
                                         nav.map((nav, index)=>/*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$framer$2d$motion$2f$dist$2f$es$2f$render$2f$components$2f$motion$2f$proxy$2e$mjs__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["motion"].div, {
                                                 initial: {
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -104,11 +104,11 @@ const portfolio = () => {
         <div className="max-w-7xl mx-auto ">
           <div className="flex justify-between items-center py-2">
             <div className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
-            <ChevronsLeftRightEllipsis  />  BG Dev
+            <ChevronsLeftRightEllipsis  />  BGarrix
             </div>
 
             {/* Desktop Navigation */}
-            <div className="hidden md:flex space-x-2 text-foreground  ">
+            <div className="hidden md:flex  text-foreground  ">
              {nav.map((nav, index) => (
               <motion.div
               initial={{ opacity: 0, y: 20 }}
